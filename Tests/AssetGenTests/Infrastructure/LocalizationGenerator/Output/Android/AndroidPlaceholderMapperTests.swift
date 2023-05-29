a812e10d760a983ffeaeb85c251e161f1481d514
Egzon Arifi <egzon.r.arifi@gmail.com>
2023-05-29 12:48:34 +0200

    Implement AndroidPlaceholderMapper
    - Unit tests

---
+++ Tests/AssetGenTests/Infrastructure/LocalizationGenerator/Output/Android/AndroidPlaceholderMapperTests.swift
@@ -0,0 +1,26 @@
+import XCTest
+@testable import AssetGen
+
+final class AndroidPlaceholderMapperTests: XCTestCase {
+  func test_mapPlaceholders_replacesIOSPlaceholderWithAndroidPlaceholder() {
+    expect(input: "Hello %@, you have %d new messages.",
+           toMapTo: "Hello %1$s, you have %2$d new messages.")
+  }
+  
+  func test_mapPlaceholders_replacesNewlineWithEscapedNewline() {
+    expect(input: "Hello\nWorld", toMapTo: "Hello\\nWorld")
+  }
+
+  func test_mapPlaceholders_handlesMultiplePlaceholders() {
+    expect(input: "Hello %@, You have %d new messages and your score is %f",
+           toMapTo: "Hello %1$s, You have %2$d new messages and your score is %3$f")
+  }
+}
+
+private extension AndroidPlaceholderMapperTests {
+  func expect(input: String, toMapTo expectedOutput: String, file: StaticString = #file, line: UInt = #line) {
+    let sut = AndroidPlaceholderMapper()
+    let output = sut.mapPlaceholders(input)
+    XCTAssertEqual(output, expectedOutput, file: file, line: line)
+  }
+}
