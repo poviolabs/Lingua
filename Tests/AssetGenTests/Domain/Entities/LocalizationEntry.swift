2231ac03ac52d84c2427d0ca46eda006eec9bb75
Egzon Arifi <egzon.r.arifi@gmail.com>
2023-05-30 13:59:42 +0200

    LocalizationModule Integration Tests

--- Tests/AssetGenTests/Domain/Entities/LocalizationEntry.swift
+++ Tests/AssetGenTests/Domain/Entities/LocalizationEntry.swift
@@ -2,13 +2,15 @@ import Foundation
 @testable import AssetGen
 
 extension LocalizationEntry {
-  static func create(plural: Bool) -> LocalizationEntry {
+  static func create(section: String = "section1",
+                     key: String = "key1",
+                     plural: Bool) -> LocalizationEntry {
     var translations = [PluralCategory.one.rawValue: "translationOne"]
     if plural {
       translations.updateValue("translationOther", forKey: PluralCategory.other.rawValue)
     }
-    return LocalizationEntry(section: "section1",
-                             key: "key1",
+    return LocalizationEntry(section: section,
+                             key: key,
                              translations: translations)
   }
 }
