5c06cab96d439d7564d767b87a5df512a03c311f
Egzon Arifi <EgzonArifi@users.noreply.github.com>
2023-06-13 16:03:42 +0200

    Omit bin.install_symlink (#72)

--- Formula/assetgen.rb
+++ Formula/assetgen.rb
@@ -1,15 +1,15 @@
-class AssetGen < Formula
+class Assetgen < Formula
   desc "Unified localization management tool for iOS & Android via Google Sheets"
   homepage "https://github.com/poviolabs/AssetGen"
   url "https://github.com/poviolabs/AssetGen/archive/0.1.0.tar.gz"
   sha256 "cf0b2a3577fe5dbe16431e00c961b768765576ffce2367e6969e558f193144cd"
 
+  depends_on :macos
   depends_on :xcode => ["14.1", :build]
 
   def install
     system "swift", "build", "--disable-sandbox", "--configuration", "release", "--disable-automatic-resolution"
     bin.install '.build/release/AssetGen'
-    bin.install_symlink bin/"AssetGen" => "assetgen"
   end
 
   test do
