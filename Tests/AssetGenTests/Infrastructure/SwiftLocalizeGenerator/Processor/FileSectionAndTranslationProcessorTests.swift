292b65d1139f26e432c1af24549cd1eb43953345
Egzon Arifi <egzon.r.arifi@gmail.com>
2023-06-15 10:35:21 +0200

    Implement FileSectionAndTranslationProcessor
    - Unit tests, mocks

---
+++ Tests/AssetGenTests/Infrastructure/SwiftLocalizeGenerator/Processor/FileSectionAndTranslationProcessorTests.swift
@@ -0,0 +1,24 @@
+import XCTest
+@testable import AssetGen
+
+final class FileSectionAndTranslationProcessorTests: XCTestCase {
+  func test_processAndMergeFiles_mergesResultsFromAllFileProcessors() {
+    let fileManagerMock = MockFileManager(files: ["file1", "file2", "file1"])
+    let sut = makeSUT(fileManagerMock: fileManagerMock)
+    let path = "testDirectoryPath"
+    
+    
+    let result = sut.processAndMergeFiles(at: path)
+    
+    XCTAssertEqual(result.sections, ["file1": Set(["section1"]), "file2": Set(["section1"])])
+    XCTAssertEqual(result.translations, ["translation1": "translationValue1"])
+  }
+}
+
+private extension FileSectionAndTranslationProcessorTests {
+  func makeSUT(fileManagerMock: FileManager = FileManager.default,
+               fileProcessors: [FileProcessor] = [MockFileProcessor()]) -> FileSectionAndTranslationProcessor {
+     let fileManagerProviderMock = MockFileManagerProvider(fileManagerMock: fileManagerMock)
+     return FileSectionAndTranslationProcessor(fileManagerProvider: fileManagerProviderMock, fileProcessors: fileProcessors)
+   }
+}
---
+++ Tests/AssetGenTests/Infrastructure/SwiftLocalizeGenerator/Processor/Mock/MockFileManagerProvider.swift
@@ -0,0 +1,14 @@
+import Foundation
+@testable import AssetGen
+
+class MockFileManagerProvider: FileManagerProvider {
+  var manager: FileManager
+  
+  init(fileManagerMock: FileManager) {
+    self.manager = fileManagerMock
+  }
+  
+  var fileManager: FileManager {
+    return manager
+  }
+}
