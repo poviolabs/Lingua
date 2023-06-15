ecc8d261b88ea4400f01159e54a1dcd98c41c5ef
Egzon Arifi <EgzonArifi@users.noreply.github.com>
2023-06-15 09:35:48 +0200

    Implement StringsDictFileProcessor (#76)

---
+++ Tests/AssetGenTests/Infrastructure/SwiftLocalizeGenerator/PlaceholderExtractor/PlaceholderExtractorTests.swift
@@ -0,0 +1,40 @@
+import XCTest
+@testable import AssetGen
+
+final class PlaceholderExtractorTests: XCTestCase {
+  func test_extractPlaceholders_withRegexPlaceholderExtractor() {
+    let extractor = PlaceholderExtractor(strategy: RegexPlaceholderExtractor())
+    verifyPlaceholders(extractor: extractor)
+  }
+  
+  func test_extractPlaceholders_withNSRegularExpressionStrategy() {
+    let extractor = PlaceholderExtractor(strategy: NSRegularExpressionPlaceholderExtractor())
+    verifyPlaceholders(extractor: extractor)
+  }
+  
+  func test_extractPlaceholders_withDefaultStrategy() {
+    let extractor = PlaceholderExtractor.make()
+    verifyPlaceholders(extractor: extractor)
+  }
+}
+ 
+private extension PlaceholderExtractorTests {
+  func verifyPlaceholders(extractor: PlaceholderExtractor) {
+    let translation = "Hello %@, you have %d unread messages and %f new notifications and %a."
+    let placeholders = extractor.extractPlaceholders(from: translation)
+    
+    XCTAssertEqual(placeholders.count, 4)
+    
+    XCTAssertEqual(placeholders[0].index, 0)
+    XCTAssertEqual(placeholders[0].type.asDataType, "String")
+    
+    XCTAssertEqual(placeholders[1].index, 1)
+    XCTAssertEqual(placeholders[1].type.asDataType, "Int")
+    
+    XCTAssertEqual(placeholders[2].index, 2)
+    XCTAssertEqual(placeholders[2].type.asDataType, "Double")
+    
+    XCTAssertEqual(placeholders[3].index, 3)
+    XCTAssertEqual(placeholders[3].type.asDataType, "Any")
+  }
+}
--- Tests/AssetGenTests/Infrastructure/SwiftLocalizeGenerator/Processor/StringsFileProcessorTests.swift
+++ Tests/AssetGenTests/Infrastructure/SwiftLocalizeGenerator/Processor/StringsFileProcessorTests.swift
@@ -50,14 +50,4 @@ private extension StringsFileProcessorTests {
   func makeSUT() -> StringsFileProcessor {
     StringsFileProcessor()
   }
-  
-  func createTemporaryFile(withContent content: String) throws -> URL {
-    let temporaryDirectory = FileManager.default.temporaryDirectory
-    let fileName = UUID().uuidString
-    let fileURL = temporaryDirectory.appendingPathComponent(fileName)
-    
-    try content.write(to: fileURL, atomically: true, encoding: .utf8)
-    
-    return fileURL
-  }
 }
