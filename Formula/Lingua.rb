61690e0f4ea9e1507d461b4b8e8bd07f4d05f463
Egzon Arifi <egzon.r.arifi@gmail.com>
2023-06-22 17:05:45 +0200

    Change sha256

--- Formula/Lingua.rb
+++ Formula/Lingua.rb
@@ -2,7 +2,7 @@ class Lingua < Formula
   desc "Unified localization management tool for iOS & Android via Google Sheets"
   homepage "https://github.com/poviolabs/Lingua"
   url "https://github.com/poviolabs/Lingua/archive/0.2.1.tar.gz"
-  sha256 "8288b4702a4a42494dbdd5aedb01884a340653f7dcfd99bc4da33d441c14d555"
+  sha256 "8ea51c173e5c4edefb520798a616a829c34b6841671f60f636d49f291e72cdd1"
 
   depends_on :macos
   depends_on :xcode => ["14.1", :build]
