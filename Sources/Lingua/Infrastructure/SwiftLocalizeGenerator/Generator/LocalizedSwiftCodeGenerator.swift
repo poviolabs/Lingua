07ff5cc3c1278fc6512f627c0bad2b5f670f9a30
Egzon Arifi <egzon.r.arifi@gmail.com>
2023-07-20 11:16:00 +0200

    Add Documentation Comments to All Members of Lingua.swift

--- Sources/Lingua/Infrastructure/SwiftLocalizeGenerator/Generator/LocalizedSwiftCodeGenerator.swift
+++ Sources/Lingua/Infrastructure/SwiftLocalizeGenerator/Generator/LocalizedSwiftCodeGenerator.swift
@@ -11,20 +11,20 @@ struct LocalizedSwiftCodeGenerator: LocalizedSwiftCodeGenerating {
     let placeholders = placeholderExtractor.extractPlaceholders(from: translation)
     
     guard placeholders.isEmpty else {
-      return generateFunctionCode(section: section, key: key, placeholders: placeholders)
+      return generateFunctionCode(section: section, key: key, translation: translation, placeholders: placeholders)
     }
     
-    return generateStaticPropertyCode(section: section, key: key)
+    return generateStaticPropertyCode(section: section, key: key, translation: translation)
   }
 }
 
 private extension LocalizedSwiftCodeGenerator {
-  func generateStaticPropertyCode(section: String, key: String) -> String {
-    "static let \(key.formatKey()) = tr(\"\(section)\", \"\(key)\")"
+  func generateStaticPropertyCode(section: String, key: String, translation: String) -> String {
+    translation.commented() + "\n\t\tstatic let \(key.formatKey()) = tr(\"\(section)\", \"\(key)\")"
   }
   
-  func generateFunctionCode(section: String, key: String, placeholders: [Placeholder]) -> String {
-    var function = "static func \(key.formatKey())("
+  func generateFunctionCode(section: String, key: String, translation: String, placeholders: [Placeholder]) -> String {
+    var function = translation.commented() + "\n\t\tstatic func \(key.formatKey())("
     
     for (index, placeholder) in placeholders.enumerated() {
       if index > 0 { function += ", " }
@@ -41,5 +41,13 @@ private extension LocalizedSwiftCodeGenerator {
     function += ")\n\t\t}"
     
     return function
+  }
+}
+
+private extension String {
+  func commented() -> String {
+    components(separatedBy: "\n")
+      .map { "\n\t\t/// \($0)" }
+      .joined()
   }
 }
