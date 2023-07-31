ade33c50551fe1b6f3485c301cbb7de786edefda
Egzon Arifi <EgzonArifi@users.noreply.github.com>
2023-07-31 15:55:10 +0200

    Implement Example-App for iOS (#129)

---
+++ Example-App/iOS/Example-App/Example-App/Resources/Localization/Lingua.swift
@@ -0,0 +1,30 @@
+// This file was generated with Lingua command line tool. Please do not change it!
+// Source: https://github.com/poviolabs/Lingua
+
+// swiftlint:disable all
+import Foundation
+
+enum Lingua {
+	enum General {
+		
+		/// Success
+		static let success = tr("General", "success")
+	}
+    
+	private static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {
+		let format = BundleToken.bundle.localizedString(forKey: key, value: nil, table: table)
+		return String(format: format, locale: Locale.current, arguments: args)
+	}
+}
+
+private final class BundleToken {
+  static let bundle: Bundle = {
+    #if SWIFT_PACKAGE
+    return Bundle.module
+    #else
+    return Bundle(for: BundleToken.self)
+    #endif
+  }()
+}
+
+// swiftlint:enable all
