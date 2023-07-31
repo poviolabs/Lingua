4f8e9e05919ce272f9a3fd9b7edb603199594d3d
Egzon Arifi <EgzonArifi@users.noreply.github.com>
2023-07-31 16:05:28 +0200

    Implement Example-App for Android (#130)

---
+++ Example-App/Android/app/src/main/java/com/lingua/example_app/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.lingua.example_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.lingua.example_app.ui.theme.ExampleAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ExampleAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting(getString(R.string.general_success))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ExampleAppTheme {
+        Greeting("Android")
+    }
+}
---
+++ Example-App/Android/scripts/cicd_localization.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# JSON file path
+JSON_FILE="./lingua_config.json"
+
+# Backup original JSON file
+cp $JSON_FILE "$JSON_FILE.bak"
+
+# Update GOOGLE_API_KEY
+sed -i '' "s|<google_api_key>|${GOOGLE_API_KEY}|g" $JSON_FILE
+
+# Update GOOGLE_SPREADSHEET_ID
+sed -i '' "s|<google_spreadsheet_id>|${GOOGLE_SPREADSHEET_ID}|g" $JSON_FILE
+
+# Print the updated JSON file
+cat $JSON_FILE
+
+# Run the lingua command
+lingua android ./lingua_config.json
+
+# Restore the original JSON file
+mv "$JSON_FILE.bak" $JSON_FILE
