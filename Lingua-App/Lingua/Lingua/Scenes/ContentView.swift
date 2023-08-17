70ce230fd6c319f0d7f5e034ec04ac349960b4b7
Egzon Arifi <egzon.r.arifi@gmail.com>
2023-08-17 15:35:19 +0200

    Create Lingua macOS app

---
+++ Lingua-App/Lingua/Lingua/Scenes/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  Lingua
+//
+//  Created by Egzon Arifi on 17/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Lingua")
+      .padding()
+  }
+}
