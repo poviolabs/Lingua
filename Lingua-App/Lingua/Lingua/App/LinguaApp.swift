52fcb38814acf1f53e2bb4d6220d6e59dfac89df
Egzon Arifi <EgzonArifi@users.noreply.github.com>
2023-08-18 14:25:57 +0200

    Setup macOS app and integrate LinguaLib as a local SPM dependency (#162)

---
+++ Lingua-App/Lingua/Lingua/App/LinguaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinguaApp.swift
+//  Lingua
+//
+//  Created by Egzon Arifi on 17/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct LinguaApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
