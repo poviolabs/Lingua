6145a20ed440b90922ff2fe7f42da99ffdf6f66d
Borut Tomazin <borut@povio.com>
2023-09-22 22:43:08 +0200

    Localized about menu.

--- Lingua-App/Lingua/Lingua/LinguaApp.swift
+++ Lingua-App/Lingua/Lingua/LinguaApp.swift
@@ -13,5 +13,25 @@ struct LinguaApp: App {
     WindowGroup {
       ContentView()
     }
+    .commands {
+      CommandGroup(replacing: .appInfo) {
+        Button(Lingua.App.about) {
+          NSApplication.shared.orderFrontStandardAboutPanel(
+            options: [
+              NSApplication.AboutPanelOptionKey.credits: NSAttributedString(
+                string: Lingua.App.description,
+                attributes: [
+                  NSAttributedString.Key.font: NSFont.boldSystemFont(
+                    ofSize: NSFont.smallSystemFontSize)
+                ]
+              ),
+              NSApplication.AboutPanelOptionKey(
+                rawValue: Lingua.App.copyright
+              ): Lingua.App.copyrightYear
+            ]
+          )
+        }
+      }
+    }
   }
 }
--- Lingua-App/Lingua/Lingua/Resources/Localization/Lingua.swift
+++ Lingua-App/Lingua/Lingua/Resources/Localization/Lingua.swift
@@ -5,6 +5,13 @@
 import Foundation
 
 enum Lingua {
+  enum App {
+    static let about = tr("App", "about")
+    static let description = tr("App", "description")
+    static let copyright = tr("App", "copyright")
+    static let copyrightYear = tr("App", "copyright_year")
+  }
+  
 	enum General {
 		/// Choose
 		static let choose = tr("General", "choose")
