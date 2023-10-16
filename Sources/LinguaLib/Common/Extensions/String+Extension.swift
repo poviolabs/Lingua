5666e9ee3ee31a92f92cd4b63cb71cb97c4184e1
Egzon Arifi <egzon.r.arifi@gmail.com>
2023-10-16 09:58:32 +0200

    bump version

--- Sources/LinguaLib/Common/Extensions/String+Extension.swift
+++ Sources/LinguaLib/Common/Extensions/String+Extension.swift
@@ -2,7 +2,7 @@ import Foundation
 
 public extension String {
   static let packageName = "Lingua"
-  static let version = "0.2.6"
+  static let version = "1.0.1"
   static let swiftLocalizedName = "\(String.packageName).swift"
   static let fileHeader = """
   This file was generated with Lingua command line tool. Please do not change it!
