59321088dabcf42605ed0bf4ad5d6c049afe95ce
Yll Fejziu <yllfejziu@gmail.com>
2023-11-28 16:19:53 +0100

    Revert tests.yml changes

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
   name: "Lingua",
   platforms: [
-    .macOS(.v13),
+    .macOS(.v12),
   ],
   products: [
     .library(name: "LinguaLib", targets: ["LinguaLib"]),
