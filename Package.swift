af94af4bcd38c2c8babd3230ca9271f3fe3b9c02
Yll Fejziu <38266916+yllfejziu@users.noreply.github.com>
2023-11-28 14:56:27 +0100

    Update tests.yml

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
   name: "Lingua",
   platforms: [
-    .macOS(.v12),
+    .macOS(.v13),
   ],
   products: [
     .library(name: "LinguaLib", targets: ["LinguaLib"]),
